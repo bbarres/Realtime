06e4e6e2610d7cf1c6fb55c577099db8cdce5a8f
bbarres <benoit.barres@gmail.com>
2021-08-23 18:54:37 +0200

    adapting and cleaning the code for the kinship matrices

--- RealTime_kinship.R
+++ RealTime_kinship.R
@@ -1,44 +1,41 @@
-###############################################################################
-###############################################################################
+##############################################################################/
+##############################################################################/
 #Preparing Kinship matrices for the Mixed linear model analysis
-###############################################################################
-###############################################################################
+##############################################################################/
+##############################################################################/
 
 #loading the libraries
 library(kinship2)
 
-#set the working directory
-setwd("~/work/Rfichiers/Githuber/Realtime_data")
 
+##############################################################################/
+#Kinship matrix based on the pedigree####
+##############################################################################/
 
-###############################################################################
-#Kinship matrix based on the pedigree
-###############################################################################
-
-pedifile<-read.table("pedfile.txt",header=T)
+pedifile<-read.table("data/pedfile.txt",header=T)
 kinMORG<-2*kinship(id=pedifile$id,mom=pedifile$mother,dad=pedifile$father)
-write.table(kinMORG,file="kinMORG.txt",sep='\t')
+write.table(kinMORG,file="output/kinMORG.txt",sep='\t')
 
 #you can also compute matrices for subsets of the entire dataset
-pedifile_nat<-read.table("pedfile_nat.txt",header=T)
+pedifile_nat<-read.table("data/pedfile_nat.txt",header=T)
 kinMORG_nat<-2*kinship(id=pedifile_nat$id,mom=pedifile_nat$mother,
                        dad=pedifile_nat$father)
-write.table(kinMORG_nat,file="kinMORG_nat.txt",sep='\t')
+write.table(kinMORG_nat,file="output/kinMORG_nat.txt",sep='\t')
 
-pedifile_lim<-read.table("pedfile_lim.txt",header=T)
+pedifile_lim<-read.table("data/pedfile_lim.txt",header=T)
 kinMORG_lim<-2*kinship(id=pedifile_lim$id,mom=pedifile_lim$mother,
                        dad=pedifile_lim$father)
-write.table(kinMORG_lim,file="kinMORG_lim.txt",sep='\t')
+write.table(kinMORG_lim,file="output/kinMORG_lim.txt",sep='\t')
 
-pedifile_renf<-read.table("pedfile_renf.txt",header=T)
+pedifile_renf<-read.table("data/pedfile_renf.txt",header=T)
 kinMORG_renf<-2*kinship(id=pedifile_renf$id,mom=pedifile_renf$mother,
                         dad=pedifile_renf$father)
-write.table(kinMORG_renf,file="kinMORG_renf.txt",sep='\t')
+write.table(kinMORG_renf,file="output/kinMORG_renf.txt",sep='\t')
 
 
-###############################################################################
-#Kinship matrix based on the pedigree
-###############################################################################
+##############################################################################/
+#Kinship matrix based on the genetic markers####
+##############################################################################/
 
 #Here are some codes to transform the output of the Spagedi 1.5a software into 
 #a matrix suitable for the analysis in TASSEL. For that you just need to run 
@@ -62,30 +59,34 @@ insertinbreed<-function(kin,inbreed)
 #We process the file for the 'lim' treatment, using a kinship computed with 
 #610 SNPs with a LD lower than 0.8 and a MAF>0.05
 #first we load the kinship matrix
-kinLOISlim<-read.table("lim_ldmaf_LOIS.txt",header=TRUE,row.names=1,sep="\t")
-#then we load the inbreeding coefficient and turn it into 0.5*(1+inbreed coeff)
-kinintra_lim<-read.table("lim_ldmaf_intraLOIS.txt",header=T,sep="\t")
+kinLOISlim<-read.table("data/lim_ldmaf_LOIS.txt",
+                       header=TRUE,row.names=1,sep="\t")
+#then we load the inbreeding coefficient and turn it into 
+#0.5*(1+inbreed coeff)
+kinintra_lim<-read.table("data/lim_ldmaf_intraLOIS.txt",header=T,sep="\t")
 kinintra_lim<-cbind(kinintra_lim,"intrakin"=0.5*(1+kinintra_lim$ALL.LOCI))
 #finally we used the function to include the inbreeding coefficient value in 
 #the matrix and export the dataset to be used in TASSEL
 transLOISlim<-2*insertinbreed(kinLOISlim,kinintra_lim)
-write.table(transLOISlim,file="snp_LIM_kinLOIS.txt")
+write.table(transLOISlim,file="output/snp_LIM_kinLOIS.txt")
 
 #We process the file for the 'lim' treatment, using a kinship computed with 
 #610 SNPs with a LD lower than 0.8 and a MAF>0.05
 #first we load the kinship matrix
-kinLOISnatrenf<-read.table("natrenf_ldmaf_LOIS.txt",header=TRUE,
-                           row.names=1,sep="\t")
-#then we load the inbreeding coefficient and turn it into 0.5*(1+inbreed coeff)
-kinintra_natrenf<-read.table("natrenf_ldmaf_intraLOIS.txt",header=T,sep="\t")
+kinLOISnatrenf<-read.table("data/natrenf_ldmaf_LOIS.txt",
+                           header=TRUE,row.names=1,sep="\t")
+#then we load the inbreeding coefficient and turn it into 
+#0.5*(1+inbreed coeff)
+kinintra_natrenf<-read.table("data/natrenf_ldmaf_intraLOIS.txt",
+                             header=T,sep="\t")
 kinintra_natrenf<-cbind(kinintra_natrenf,
                         "intrakin"=0.5*(1+kinintra_natrenf$ALL.LOCI))
 #finally we used the function to include the inbreeding coefficient value in 
 #the matrix and export the dataset to be used in TASSEL
 transLOISnatrenf<-2*insertinbreed(kinLOISnatrenf,kinintra_natrenf)
-write.table(transLOISnatrenf,file="snp_NATRENF_kinLOIS.txt")
+write.table(transLOISnatrenf,file="output/snp_NATRENF_kinLOIS.txt")
 
 
-###############################################################################
+##############################################################################/
 #END
-###############################################################################
+##############################################################################/
