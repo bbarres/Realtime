fa3a575573036ed574a75e045feb0f5171f65e92
bbarres <benoit.barres@gmail.com>
2021-11-10 18:05:11 +0100

    importing the data to a genelight object

--- RealTime_Fstcomparison.R
+++ RealTime_Fstcomparison.R
@@ -5,17 +5,25 @@
 ##############################################################################/
 
 
+library(adegenet)
+first<-read.snp("data/data.snp")
+
+meta<-read.table("data/metadatIND.snp",sep="\t",header=T,
+                 stringsAsFactors=TRUE,na.strings="..")
+
+indNames(first)<-meta$nom_Ind
+first@pop<-meta$famille
+first@other$mother<-meta$mother
+first@other$father<-meta$Father
+first@other$traitement<-meta$traitement
+first@loc.names<-as.character(meta_mark$loc.names)
+second<-first[first$pop!="PAR" & first$pop!="CC" & first$pop!="A4"]
 
 
+first$pop
 
 
-
-
-
-
-
-
-
+meta
 
 
 
