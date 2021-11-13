c575be6e86ad910028336ac304509cb8c9cdb6e5
bbarres <benoit.barres@gmail.com>
2021-11-13 23:42:15 +0100

    reorg of the script

--- RealTime_Fstcomparison.R
+++ RealTime_Fstcomparison.R
@@ -6,26 +6,39 @@
 
 
 library(adegenet)
-first<-read.snp("data/data.snp")
-
-meta<-read.table("data/metadatIND.snp",sep="\t",header=T,
-                 stringsAsFactors=TRUE,na.strings="..")
-
-indNames(first)<-meta$nom_Ind
-first@pop<-meta$famille
-first@other$mother<-meta$mother
-first@other$father<-meta$Father
-first@other$traitement<-meta$traitement
-first@loc.names<-as.character(meta_mark$loc.names)
-second<-first[first$pop!="PAR" & first$pop!="CC" & first$pop!="A4"]
+library(hierfstat)
+library(poppr)
 
 
-first$pop
+##############################################################################/
+#using the dosage format for the snp data
+##############################################################################/
+
+compSNP<-read.snp("data/data.snp")
+metaIND<-read.table("data/metadatIND.snp",sep="\t",header=T,
+                    stringsAsFactors=TRUE,na.strings="..")
+metaMARK<-read.table("data/metadatMARK.snp",sep="\t",header=T,
+                     stringsAsFactors=TRUE,na.strings="..")
+compSNP@loc.names<-as.character(metaMARK$loc.names)
+#adding other information to the @other slot for individuals
+indNames(compSNP)<-metaIND$nom_Ind
+compSNP@other$famille<-metaIND$famille
+compSNP@other$mother<-metaIND$mother
+compSNP@other$father<-metaIND$father
+compSNP@other$traitement<-metaIND$traitement
+compSNP@other$traitsimp<-metaIND$trait_simp
+compSNP@other$live_bin<-metaIND$live_bin
 
 
-meta
+#limiting the data set to individuals of the experimental setting 
+experSNP<-compSNP[compSNP$pop!="PAR" & compSNP$pop!="CC" 
+                  & compSNP$pop!="A4" & compSNP$pop!="3P"]
 
+#some individuals don't have the information of their death
+experSNP@ind.names[experSNP@other$live_bin=="NA"]
 
+#we remove these individuals until clarification
+temp<-experSNP[experSNP@other$live_bin!="NA"]
 
 
 ##############################################################################/
