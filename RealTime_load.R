55ed9134bdff9c8800ba5beb7c94a7b73c96d0ee
bbarres <benoit.barres@gmail.com>
2021-11-20 23:26:06 +0100

    adding the importation and preparation of the final datafile

--- RealTime_load.R
+++ RealTime_load.R
@@ -11,9 +11,33 @@ library(kinship2)
 library(RColorBrewer)
 
 #loading the different data sets
-mrkrInf<-read.table()
+
+#information of the snp markers
+#mrkrInf<-read.table()
+
+#data by individuals, including all individuals (hd, families, parents and CC
 RTdata<-read.table("data/datatot.txt",sep="\t",stringsAsFactors=TRUE,
                    header=TRUE)
+#reordering the family levels
+RTdata$family_simp<-factor(RTdata$family_simp,
+                           levels=c("1","9","11","26","27","45","48",
+                                    "51","70","71","72","73","74","75",
+                                    "76","77","99","CC","PAR","hd"))
+
+#limiting the data set to the experimental set up
+dispo<-RTdata[RTdata$family_simp!="PAR",]
+
+#data subset of individuals with microsatellite data. There is a total of 
+#12 markers, but 3 were found to be of limited quality (too many missing
+#data). Therefore the quality criteria to remove individuals is a maximum 
+#of 2 microsatellite missing data for the 9 best microsatellite
+micro.dat<-RTdata[RTdata$na.9micro<3 & !is.na(RTdata$na.9micro),]
+
+#data subset of individuals with snp data. The quality of snp genotyping
+#as been assessed beforehand and can be sorted with the 'Quality_SNPage' 
+#column
+snp.dat<-RTdata[RTdata$SNPage==1 & !is.na(RTdata$SNPage) 
+                & RTdata$Quality_SNPage==1,]
 
 
 ##############################################################################/
--- RealTime_plot_map.R
+++ RealTime_plot_map.R
@@ -4,9 +4,26 @@
 ##############################################################################/
 ##############################################################################/
 
+source("RealTime_load.R")
+
+
 ##############################################################################/
-#load the data####
+#map of the experimental set up####
 ##############################################################################/
+
+#how many individuals in each treatment for each family
+table(dispo$family_simp,dispo$trait)
+
+colovec<-c(brewer.pal(12,"Paired"),brewer.pal(8,"Dark2")[c(1,4,8)])
+colovec<-c(colovec[1:3],"#ffffff",colovec[4:15],"#ffffff","#000000",
+           "#ffffff","#ffffff")
+op<-par(mar=c(0,0,10,0))
+plot(dispo$coord_X,dispo$coord_Y,bty="n",ann=FALSE,axes=FALSE,
+     pch=c(rep(21,17),24,21,21)[as.numeric(dispo$family_simp)],
+     bg=colovec[as.numeric(dispo$family_simp)])
+par(op)
+#export to .pdf 20 x 10 inches
+
 
 #map of the different families
 plan<-read.table("data/tout.txt")
