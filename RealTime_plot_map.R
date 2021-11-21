e551ae4f1fe2e6d3e179ff5b28a4206033f7219f
bbarres <benoit.barres@gmail.com>
2021-11-21 01:12:23 +0100

    map of the experimental setup

--- RealTime_plot_map.R
+++ RealTime_plot_map.R
@@ -17,12 +17,26 @@ table(dispo$family_simp,dispo$trait)
 colovec<-c(brewer.pal(12,"Paired"),brewer.pal(8,"Dark2")[c(1,4,8)])
 colovec<-c(colovec[1:3],"#ffffff",colovec[4:15],"#ffffff","#000000",
            "#ffffff","#ffffff")
-op<-par(mar=c(0,0,10,0))
+op<-par(mar=c(0,0,15,0))
 plot(dispo$coord_X,dispo$coord_Y,bty="n",ann=FALSE,axes=FALSE,
      pch=c(rep(21,17),24,21,21)[as.numeric(dispo$family_simp)],
      bg=colovec[as.numeric(dispo$family_simp)])
+legend(x=300,y=950,horiz=TRUE,x.intersp=0.2,xpd=TRUE,pt.cex=2,
+       text.width=seq(from=0,to=1,by=0.06),bty="n",
+       legend=as.character(levels(dispo$family_simp))[c(1:3,5:16,18,20)],
+       pch=c(rep(21,15),24,21),
+       pt.bg=colovec[-c(4,17)])
+title(main="Map of the experimental setup",cex.main=4,line=10)
 par(op)
 #export to .pdf 20 x 10 inches
+
+
+
+
+
+##############################################################################/
+#END
+##############################################################################/
 
 
 #map of the different families
@@ -52,8 +66,3 @@ points(bord, pch=21, bg="green4", col="black")
 points(fam_3P, pch=20, bg="blue", col="orange")
 
 #export to .pdf 17 x 10 inches
-
-
-##############################################################################/
-#END
-##############################################################################/
