5d1ab66bbfef65bd6887e3e776f080539b6ff860
bbarres <benoit.barres@gmail.com>
2022-05-30 18:53:27 +0200

    separating oidium traits from other traits

--- RealTime_GWAS.R
+++ RealTime_GWAS.R
@@ -21,16 +21,37 @@ ToutTrait<-read.table("pheno_final.txt",header=TRUE)
 NatG<-read.delim("nat.hmp.txt",header=FALSE)
 NatLois<-read.table("nat_lois_mat.txt", head = FALSE)
 
-#Step 2: Run GAPIT
+#Blink method on oidium phenotype
 natGAPIT<-GAPIT(
-  Y=ToutTrait,
+  Y=ToutTrait[,1:7],
   G=NatG,
   kinship.algorithm="Loiselle",
   #KI=NatLois,
   PCA.total=0,
   model="Blink"
+  #,Random.model=TRUE
 )
 
+#Blink method on general phenotype
+natGAPIT<-GAPIT(
+  Y=ToutTrait[,c(1,7:10)],
+  G=NatG,
+  kinship.algorithm="Loiselle",
+  #KI=NatLois,
+  PCA.total=0,
+  model="Blink"
+  #,Random.model=TRUE
+)
+
+#MLM method on oidium trait
+natGAPIT<-GAPIT(
+  Y=ToutTrait[,1:7],
+  G=NatG,
+  kinship.algorithm="Loiselle",
+  #KI=NatLois,
+  PCA.total=0,
+  model="MLM"
+)
 
 ##############################################################################/
 #first attempt without Q and kinship specific matrices fot natural condition###
@@ -40,14 +61,26 @@ ToutTrait<-read.table("pheno_final.txt",header=TRUE)
 LimG<-read.delim("lim.hmp.txt",header=FALSE)
 LimLois<-read.table("lim_lois_mat.txt", head = FALSE)
 
-#Step 2: Run GAPIT
+#Blink method on oidium phenotype
 limGAPIT<-GAPIT(
-  Y=ToutTrait,
+  Y=ToutTrait[,1:7],
   G=LimG,
   kinship.algorithm="Loiselle",
   #KI=LimLois,
   PCA.total=0,
   model="Blink"
+  #,Random.model=TRUE
+)
+
+#Blink method on general phenotype
+limGAPIT<-GAPIT(
+  Y=ToutTrait[,c(1,7:10)],
+  G=LimG,
+  kinship.algorithm="Loiselle",
+  #KI=LimLois,
+  PCA.total=0,
+  model="Blink"
+  #,Random.model=TRUE
 )
 
 
