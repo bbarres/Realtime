587d73c12a69fa0d24f09130f92810fb44fa10ef
bbarres <benoit.barres@gmail.com>
2022-07-02 02:23:29 +0200

    reorg code for figure by SNP

--- RealTime_GWAS.R
+++ RealTime_GWAS.R
@@ -93,7 +93,7 @@ barplot(kdrDistr$n,ylim=c(0,1100),
         space=c(rep(0.1,3),1.4,rep(0.1,2),1.4,
                 rep(0.1,2),1.4,rep(0.1,2)),
         font.lab=2,add=TRUE)
-axis(1,at=graf[c(2,5,8,11)],labels=FALSE, lwd=4)
+axis(1,at=graf[c(2,5,8,11)],labels=FALSE,lwd=4)
 axis(2,at=c(50,100,200,400,600,800,1000),
      labels=c(50,100,200,400,600,800,1000),lwd=4,las=1,font=2,cex.axis=1.1)
 box(bty="l",lwd=4)
@@ -150,16 +150,26 @@ temp<-temp[,c(1,48:866)]
 temp2<-temp[,colnames(temp)=="Sample_ID" | 
               colnames(temp)==RezNatGAPIT$SNP[1]]
 temp3<-merge(temp2,NatTrait,by.x="Sample_ID",by.y="Taxa")
-barplot(as.data.frame(table(temp3$`Dead or Alive`,
-                            temp3$Entomo_CL7647CT8535_01_89))$Freq,
-        col=brewer.pal(8,"Dark2")[2:1],las=1,
-        space=c(0.1,rep(c(0.1,1.4),2),0.1))
 
+colnames(temp3)
 
-boxplot(temp3$oid_moy~temp3$Entomo_CL7647CT8535_01_89,boxwex=0.3,las=1)
-stripchart(temp3$oid_moy~temp3$Entomo_CL7647CT8535_01_89,
+graf<-barplot(as.data.frame(table(temp3$`Dead or Alive`,
+                                  temp3$Entomo_CL7647CT8535_01_89))$Freq,
+              col=brewer.pal(8,"Dark2")[2:1],las=1,
+              space=c(0.1,rep(c(0.1,0.9),2),0.1),xaxt="n",yaxt="n")
+abline(h=c(50,100,200,300),col=grey(0.8,0.8),lwd=2,lty=1)
+axis(1,at=(graf[c(1,3,5)]+graf[c(2,4,6)])/2,labels=FALSE,lwd=4)
+axis(2,lwd=4,las=1,font=2,cex.axis=1.1)
+box(bty="l",lwd=4)
+mtext(colnames(table(temp3$`Dead or Alive`,
+                     temp3$Entomo_CL7647CT8535_01_89)),
+      at=(graf[c(1,3,5)]+graf[c(2,4,6)])/2,
+      line=1.5,cex=1.4,side=1,font=2)
+
+boxplot(temp3$`Powdery mildew`~temp3$Entomo_CL7647CT8535_01_89,boxwex=0.3,las=1)
+stripchart(temp3$`Powdery mildew`~temp3$Entomo_CL7647CT8535_01_89,
            method="jitter",col="red",vertical=TRUE,pch=19,cex=0.7,add=TRUE)
-vioplot(temp3$oid_moy~temp3$Entomo_CL7647CT8535_01_89,boxwex=0.3,las=1)
+vioplot(temp3$`Powdery mildew`~temp3$Entomo_CL7647CT8535_01_89,boxwex=0.3,las=1)
 
 table(temp3$Entomo_CL7647CT8535_01_89)
 
