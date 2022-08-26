1c10be11e7af51e82c44eb12c79bcadc67f27e77
bbarres <benoit.barres@gmail.com>
2022-08-26 13:54:52 +0200

    improving the PHt Figure

--- RealTime_GENHET.R
+++ RealTime_GENHET.R
@@ -170,11 +170,11 @@ snpGen<-df2genind(snp.dat[,temp],ploidy=2,sep="/",
                   pop=snp.dat$family_simp)
 #adding information in the 'other' slot
 snpGen@other$fam<-snp.dat$family_simp
-snpGen@other$treat<-snp.dat$exp
+snpGen@other$treat<-snp.dat$treat
 snpGen@other$height<-snp.dat$Hdeb17
-snpGen@other$DoA<-snp.dat$live_bin
+snpGen@other$DoA<-snp.dat$statut10
 #combine experimental and dead or alive information
-snpGen@other$newPop<-paste(snp.dat$exp,snp.dat$live_bin,sep="")
+snpGen@other$newPop<-paste(snp.dat$treat,snp.dat$statut10,sep="")
 locNames(snpGen)<-paste("snp",1:819,sep="")
 
 #we limit the data set to the individuals belonging to the experimental
@@ -262,71 +262,78 @@ HetVivMortLim<-HetVivMortLim[HetVivMortLim$fam!="Global",]
 HetVivMortLim$fam[HetVivMortLim$fam=="1"]<-"01"
 HetVivMortLim$fam[HetVivMortLim$fam=="9"]<-"09"
 
-#plot of the different indices for Exposed treatment by family
+
+
+#plot of the different indices for Natural treatment by family
 op<-par(mfrow=c(5,1))
 vioplot(as.numeric(HetVivMortExp$PHt)~HetVivMortExp$vivmor:HetVivMortExp$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Exposed / PHt")
+        main="Dead or Alive: Natural / PHt")
 vioplot(as.numeric(HetVivMortExp$Hs_obs)~
           HetVivMortExp$vivmor:HetVivMortExp$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Exposed / Hs_obs")
+        main="Dead or Alive: Natural / Hs_obs")
 vioplot(as.numeric(HetVivMortExp$Hs_exp)~
           HetVivMortExp$vivmor:HetVivMortExp$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Exposed / Hs_exp")
+        main="Dead or Alive: Natural / Hs_exp")
 vioplot(as.numeric(HetVivMortExp$IR)~HetVivMortExp$vivmor:HetVivMortExp$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Exposed / IR")
+        main="Dead or Alive: Natural / IR")
 vioplot(as.numeric(HetVivMortExp$HL)~HetVivMortExp$vivmor:HetVivMortExp$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Exposed / HL")
+        main="Dead or Alive: Natural / HL")
 par(op)
 #export to .pdf 20 x 20 inches
 
-#plot of the different indices for Limited treatment by family
+#plot of the different indices for Protected treatment by family
 op<-par(mfrow=c(5,1))
 vioplot(as.numeric(HetVivMortLim$PHt)~HetVivMortLim$vivmor:HetVivMortLim$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Limited / PHt")
+        main="Dead or Alive: Protected / PHt")
 vioplot(as.numeric(HetVivMortLim$Hs_obs)~
           HetVivMortLim$vivmor:HetVivMortLim$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Limited / Hs_obs")
+        main="Dead or Alive: Protected / Hs_obs")
 vioplot(as.numeric(HetVivMortLim$Hs_exp)~
           HetVivMortLim$vivmor:HetVivMortLim$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Limited / Hs_exp")
+        main="Dead or Alive: Protected / Hs_exp")
 vioplot(as.numeric(HetVivMortLim$IR)~HetVivMortLim$vivmor:HetVivMortLim$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Limited / IR")
+        main="Dead or Alive: Protected / IR")
 vioplot(as.numeric(HetVivMortLim$HL)~HetVivMortLim$vivmor:HetVivMortLim$fam,
         col = c("orange","yellow"),sep=":",las=1,
         ylab="Value",xlab="Pheno:Family",
-        main="Dead or Alive: Limited / HL")
+        main="Dead or Alive: Protected / HL")
 par(op)
 #export to .pdf 20 x 20 inches
 
-#plot of the PHt index for both Exposed and limited treatment by family
-op<-par(mfrow=c(2,1))
-#semi violin plot for the exposed treatment
+
+
+
+#plot of the PHt index for both natural and protected treatment by family
+op<-par(mfrow=c(2,1),mar=c(2,4,4,1))
+#semi violin plot for the natural treatment
 HetAli<-HetVivMortExp[HetVivMortExp$vivmor==1,]
 HetDea<-HetVivMortExp[HetVivMortExp$vivmor==0,]
 colovec<-c(brewer.pal(12,"Set3")[6:7],
            brewer.pal(9,"Set1")[1:2])
-vioplot(as.numeric(HetVivMortExp$PHt)~HetVivMortExp$fam,
+vioplot(as.numeric(HetVivMortExp$PHt)~HetVivMortExp$fam,xaxt="n",yaxt="n",
         col = c("transparent"),sep=":",las=1,border="transparent",
-        ylab="PHt",xlab="",ylim=c(0.07,0.35),frame.plot=FALSE,
-        lineCol="transparent",rectCol="transparent",main="Exposed treatment")
+        ylab="PHt",xlab="",ylim=c(0.07,0.35),frame.plot=FALSE,cex.main=2,
+        lineCol="transparent",rectCol="transparent",main="Natural treatment")
+axis(1,lwd=2,at=c(1:15),labels=levels(as.factor(HetVivMortExp$fam)),font=2)
+axis(2,lwd=2,las=1,font=2)
 vioplot(as.numeric(HetDea$PHt)~HetDea$fam,plotCentre="line",
         col=colovec[1],sep=":",las=1,side="left",frame.plot=FALSE,
         add=TRUE)
@@ -350,8 +357,8 @@ points(x=c(1:15)-0.2,y=aggregate(as.numeric(HetDea$PHt),
 points(x=c(1:15)+0.2,y=aggregate(as.numeric(HetAli$PHt),
                                   list(HetAli$fam),FUN=mean)[,2],
        pch=21,bg=colovec[4],cex=1.2)
-box(bty="l")
-legend(-0.5,0.43,c("dead","alive"),fill=colovec[1:2],cex=1.3,
+box(bty="l",lwd=2)
+legend(-0.2,0.43,c("dead","alive"),fill=colovec[1:2],cex=1.3,
        bty="n",x.intersp=0.2,y.intersp=0.7,xpd=TRUE)
 
 #semi violin plot for the protected treatment
@@ -359,11 +366,13 @@ HetAli<-HetVivMortLim[HetVivMortLim$vivmor==1,]
 HetDea<-HetVivMortLim[HetVivMortLim$vivmor==0,]
 colovec<-c(brewer.pal(12,"Set3")[6:7],
            brewer.pal(9,"Set1")[1:2])
-vioplot(as.numeric(HetVivMortLim$PHt)~HetVivMortLim$fam,
+vioplot(as.numeric(HetVivMortLim$PHt)~HetVivMortLim$fam,xaxt="n",yaxt="n",
         col = c("transparent"),sep=":",las=1,border="transparent",
         ylab="PHt",xlab="",ylim=c(0.07,0.35),frame.plot=FALSE,
-        lineCol="transparent",rectCol="transparent",
+        lineCol="transparent",rectCol="transparent",cex.main=2,
         main="Protected treatment")
+axis(1,lwd=2,at=c(1:15),labels=levels(as.factor(HetVivMortExp$fam)),font=2)
+axis(2,lwd=2,las=1,font=2)
 vioplot(as.numeric(HetDea$PHt)~HetDea$fam,plotCentre="line",
         col=colovec[1],sep=":",las=1,side="left",frame.plot=FALSE,
         add=TRUE)
@@ -387,9 +396,13 @@ points(x=c(1:15)-0.2,y=aggregate(as.numeric(HetDea$PHt),
 points(x=c(1:15)+0.2,y=aggregate(as.numeric(HetAli$PHt),
                                  list(HetAli$fam),FUN=mean)[,2],
        pch=21,bg=colovec[4],cex=1.2)
-box(bty="l")
+box(bty="l",lwd=2)
 par(op)
-#export to .pdf 8 x 10 inches
+#export to .pdf 10 x 10 inches
+
+
+
+
 
 #Plot for global data comparing Exposed and Limited treatments
 HetVivMortGlob<-HetVivMort[HetVivMort$fam=="Global",]
