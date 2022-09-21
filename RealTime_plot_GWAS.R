867d9cf0c8fff3c2cc1f82739f259cfc584a71d6
bbarres <benoit.barres@gmail.com>
2022-09-22 00:37:29 +0200

    minor modifications

--- RealTime_plot_GWAS.R
+++ RealTime_plot_GWAS.R
@@ -45,7 +45,8 @@ nf<-layout(matrix(c(1,2,2,2,3,3,3,4,4,4,5,5,5,
                     7,13,13,13,14,14,14,15,15,15,16,16,16,
                     8,17,17,17,18,18,18,19,19,19,20,20,20,
                     8,17,17,17,18,18,18,19,19,19,20,20,20,
-                    8,17,17,17,18,18,18,19,19,19,20,20,20),10,13,byrow=TRUE))
+                    8,17,17,17,18,18,18,19,19,19,20,20,20),
+                  10,13,byrow=TRUE))
 op<-par(mar=c(0,0,0,0),lwd=2)
 plot.new()
 plot.new()
@@ -56,10 +57,10 @@ plot.new()
 text(0.5,0.5,"Powdery mildew\ninfection",font=2,cex=2.5)
 plot.new()
 text(0.5,0.7,"Mortality rate (%)",font=2,cex=2.5)
-legend(0.2,0.55,legend=c("Natural treatment","Protected treatment"),
+legend(0.25,0.55,legend=c("Natural treatment","Protected treatment"),
        fill=c(colnat[3],colprt[3]),border=c(colnat[3],colprt[3]),
        density=c(-1,20),angle=c(0,60),bty="n",
-       cex=1,y.intersp=1,x.intersp=0.5,ncol=1,xpd=TRUE)
+       cex=1.5,y.intersp=1.3,x.intersp=0.5,ncol=1,xpd=TRUE)
 plot.new()
 text(0.5,0.5,Rezcomb[1,1],srt=90,font=2,cex=1.5)
 plot.new()
@@ -110,8 +111,8 @@ for (i in c(1,11,16)) {
           ylab="Acorn weight (g)",main="Acorn weight")
   axis(1,labels=paste(colnames(table(tempo$`Acorn weight`,tempo[,2])),
                       " (n=",table(tempo[,2]),")",
-                      sep=""),at=c(1,2,3),cex=2,lwd=2)
-  axis(2,at=seq(from=1,to=13,by=2),las=1,cex=2,lwd=2)
+                      sep=""),at=c(1,2,3),cex.axis=1.3,lwd=2)
+  axis(2,at=seq(from=1,to=13,by=2),las=1,cex.axis=1.3,lwd=2)
   box(bty="o")
   vioplot(tempo$`Height`~tempo[,2],boxwex=0.3,las=1,
           border=colo[2:4],lineCol=colo[2:4],rectCol=colo[2:4],
@@ -120,8 +121,8 @@ for (i in c(1,11,16)) {
           ylab="Height (cm)",main="Height")
   axis(1,labels=paste(colnames(table(tempo$`Height`,tempo[,2])),
                       " (n=",table(tempo[,2]),")",
-                      sep=""),at=c(1,2,3),cex=2,lwd=2)
-  axis(2,at=seq(from=5,to=115,by=15),las=1,cex=2,lwd=2)
+                      sep=""),at=c(1,2,3),cex.axis=1.3,lwd=2)
+  axis(2,at=seq(from=5,to=115,by=15),las=1,cex.axis=1.3,lwd=2)
   box(bty="o")
   vioplot(tempo$`Powdery mildew`~tempo[,2],boxwex=0.3,las=1,
           border=colo[2:4],lineCol=colo[2:4],rectCol=colo[2:4],
@@ -130,8 +131,8 @@ for (i in c(1,11,16)) {
           ylab="Powdery mildew note",main="Powdery mildew")
   axis(1,labels=paste(colnames(table(tempo$`Powdery mildew`,tempo[,2])),
                       " (n=",table(tempo[,2]),")",
-                      sep=""),at=c(1,2,3),cex=2,lwd=2)
-  axis(2,at=seq(from=0,to=70,by=10),las=1,cex=2,lwd=2)
+                      sep=""),at=c(1,2,3),cex.axis=1.3,lwd=2)
+  axis(2,at=seq(from=0,to=70,by=10),las=1,cex.axis=1.3,lwd=2)
   box(bty="o")
   graf<-barplot(propsurv$`Mortality Rate`,density=c(-1,20),angle=c(0,60),
                 col=c(colnat[2],colprt[2],colnat[3],colprt[3],
@@ -144,9 +145,9 @@ for (i in c(1,11,16)) {
                 space=c(0,0.1,rep(c(0.8,0.1),times=2)),font.lab=2)
   text(labels=paste("n=",totaleff$`Total number`,sep=""),
        x=graf,y=propsurv$`Mortality Rate`+5,cex=1.1,font=3,srt=0)
-  axis(1,at=graf[seq(1,6,2)]+0.55,cex=2,lwd=2,
+  axis(1,at=graf[seq(1,6,2)]+0.55,cex.axis=1.3,lwd=2,
        colnames(table(tempo$`Survival`,tempo[,2])))
-  axis(2,at=seq(from=0,to=100,by=20),las=1,cex=2,lwd=2)
+  axis(2,at=seq(from=0,to=100,by=20),las=1,cex.axis=1.3,lwd=2)
   box(bty="o")
 
 }
