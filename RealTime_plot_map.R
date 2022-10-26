e20a54cc52018500a2e899d0a22d86be839fbe1b
bbarres <benoit.barres@gmail.com>
2022-10-26 23:25:06 +0200

    small correction on the supplementary figure S2, the map of the experimental setup

--- RealTime_plot_map.R
+++ RealTime_plot_map.R
@@ -17,6 +17,7 @@ table(dispo$family_simp,dispo$trait)
 colovec<-c(brewer.pal(12,"Paired"),brewer.pal(8,"Dark2")[c(1,4,8)])
 colovec<-c(colovec[1:3],"#ffffff",colovec[4:15],"#ffffff","#000000",
            "#ffffff","#ffffff")
+pdf(file="output/Figure_S2_map.pdf",width=20,height=12)
 op<-par(mar=c(0,0,15,0))
 plot(dispo$coord_X,dispo$coord_Y,bty="n",ann=FALSE,axes=FALSE,
      pch=c(rep(21,17),24,22)[as.numeric(dispo$family_simp)],
@@ -27,14 +28,15 @@ text(x=c(20,620,1220,20,620,1220,20,620,1220),
               "B1 / natural","B2 / natural","B3 / protected",
               "C1 / protected", "C2 / natural","C3 / natural"),
      xpd=TRUE,cex=2,font=2,adj=c(0,0))
-legend(x=1300,y=1050,horiz=FALSE,x.intersp=0.2,xpd=TRUE,pt.cex=2,
-       text.width=seq(from=0,to=1,by=0.06),bty="n",ncol=4,
+legend(x=1430,y=1020,horiz=FALSE,x.intersp=1,xpd=TRUE,pt.cex=2,
+       text.width=50,bty="n",ncol=4,
        legend=as.character(levels(dispo$family_simp))[c(1:3,5:16,18,19)],
-       pch=c(rep(21,15),24,22),
+       pch=c(rep(21,15),24,22),y.intersp=1.8,
        pt.bg=colovec[-c(4,17)],title="Families",title.cex=2)
 title(main="Map of the experimental setup",cex.main=4,line=10)
 par(op)
 #export to .pdf 20 x 12 inches
+dev.off()
 
 
 ##############################################################################/
@@ -48,9 +50,9 @@ plot(dispo$coord_X,dispo$coord_Y,bty="n",ann=FALSE,axes=FALSE,
      bg=colovec[as.numeric(as.factor(dispo$an_mort))])
 text(x=c(20,620,1220,20,620,1220,20,620,1220),
      y=c(760,760,760,455,455,455,145,145,145),
-     labels=c("A1 / nat","A2 / lim","A3 / renf",
-              "B1 / renf","B2 / nat","B3 / lim",
-              "C1 / lim", "C2 / renf","C3 / nat"),
+     labels=c("A1 / natural","A2 / protected","A3 / natural",
+              "B1 / natural","B2 / natural","B3 / protected",
+              "C1 / protected", "C2 / natural","C3 / natural"),
      xpd=TRUE,cex=2,font=2,adj=c(0,0))
 legend(x=230,y=950,horiz=FALSE,x.intersp=0.5,y.intersp=0.8,
        xpd=TRUE,pt.cex=3,bty="n",text.font=2,
@@ -89,9 +91,9 @@ plot(dispo$coord_X,dispo$coord_Y,bty="n",ann=FALSE,axes=FALSE,
      bg=colovec[as.numeric(dispo$an_mort)])
 text(x=c(20,620,1220,20,620,1220,20,620,1220),
      y=c(760,760,760,455,455,455,145,145,145),
-     labels=c("A1 / nat","A2 / lim","A3 / renf",
-              "B1 / renf","B2 / nat","B3 / lim",
-              "C1 / lim", "C2 / renf","C3 / nat"),
+     labels=c("A1 / natural","A2 / protected","A3 / natural",
+              "B1 / natural","B2 / natural","B3 / protected",
+              "C1 / protected", "C2 / natural","C3 / natural"),
      xpd=TRUE,cex=2,font=2,adj=c(0,0))
 legend(x=300,y=950,horiz=FALSE,x.intersp=1,y.intersp=0.5,
        xpd=TRUE,pt.cex=3,bty="n",text.font=2,
@@ -110,32 +112,3 @@ par(op)
 ##############################################################################/
 #END
 ##############################################################################/
-
-
-#map of the different families
-plan<-read.table("data/tout.txt")
-erreur<-read.table("data/etiq_dif_microsat.txt")
-bord<-read.table("data/bordure_nonechant.txt")
-plot(plan, pch=19, bg="blue")
-points(erreur, pch=19, bg="red", col="red")
-points(bord, pch=19, bg="green4", col="green4")
-
-#map of the flagshoot for years 2010 and 2011
-plan<-read.table("data/tout.txt")
-bord<-read.table("data/bordures.txt")
-drap2010<-read.table("data/drap2010.txt")
-drap2011<-read.table("data/drap2011.txt")
-plot(plan, pch=21, col="black")
-points(bord, pch=21, bg="green4", col="black")
-points(drap2010, pch=20, bg="blue", col="red2")
-points(drap2011, pch=20, bg="orange", col="orange")
-
-#map of the individuals of the 3P family
-plan<-read.table("data/tout.txt")
-bord<-read.table("data/bordures.txt")
-fam_3P<-read.table("data/3P_fam.txt")
-plot(plan, pch=21, col="black")
-points(bord, pch=21, bg="green4", col="black")
-points(fam_3P, pch=20, bg="blue", col="orange")
-
-#export to .pdf 17 x 10 inches
