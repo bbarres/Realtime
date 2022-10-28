a4395b9f52c5631a564cc2240ca7d209391b3ef9
bbarres <benoit.barres@gmail.com>
2022-10-28 18:42:37 +0200

    improving Figure 5, with 2 versions

--- RealTime_evolShannon.R
+++ RealTime_evolShannon.R
@@ -103,21 +103,56 @@ progSurv$treat<-as.factor(progSurv$treat)
 SurvProp<-as.data.frame(prop.table(table(progSurv$treat,
                                          progSurv$DoA,
                                          progSurv$family_simp),
-                     margin=c(1,3)))
+                     margin=c(1,3))*100)
 SurvProp<-SurvProp[SurvProp$Var2=="alive",]
 SurvProp<-pivot_wider(SurvProp[,-c(2)],names_from=Var1,values_from=Freq)
 colnames(SurvProp)<-c("Progeny","Natural","Protected")
 SurvProp$PMinf<-tapply(progSurv$oid_moy,INDEX=progSurv$family_simp,
                        FUN=mean,na.rm=TRUE)
 
-#ploting the results
-coloor<-brewer.pal(11,"RdYlGn")[11:1]
-as.numeric(cut(SurvProp$PMinf,breaks=11))
-plot(SurvProp[,c(3,2)],xlim=c(0,1),ylim=c(0,1),
-     las=1,pch=19,cex=3,
-     col=coloor[as.numeric(cut(SurvProp$PMinf,breaks=11))])
+#ploting the results (version 1)
+pdf(file="output/Figure5_SurvByProg.pdf",width=7,height=7)
+op<-par(mar=c(5.1,4.3,1,1))
+coloor<-brewer.pal(9,"YlOrRd") #with yellow to red gradient
+nbbrak<-c(20,21,22,23,24,25,26,27,28)
+plot(SurvProp[,c(3,2)],xlim=c(0,100),ylim=c(0,100),bty="n",
+     las=1,pch=21,cex=3,col="black",xaxt="n",yaxt="n",
+     bg=coloor[as.numeric(cut(SurvProp$PMinf,breaks=nbbrak))],
+     ylab="Natural treatment survival rate (%)",
+     xlab="Protected treatment survival rate (%)",
+     font.lab=2,cex.lab=1.5)
 text(SurvProp[,c(3,2)],labels=SurvProp$Progeny)
 abline(0,1,lty=5,col=grey(0.6,1),lwd=3)
+legend(8,98,legend=c("]20-21]","]21-22]","]22-23]","]23-24]",
+                    "]24-25]","]25-26]","]26-27]","]27-28]"),
+       bty="n",fill=coloor,title="Mean\nPM infection")
+axis(1,lwd=2,cex.axis=1,las=1,font=2)
+axis(2,lwd=2,las=1,font=2)
+box(lwd=2)
+par(op)
+dev.off()
+
+#ploting the results (version 2)
+pdf(file="output/Figure5_SurvByProg2.pdf",width=7,height=7)
+op<-par(mar=c(5.1,4.3,1,1))
+coloor<-brewer.pal(11,"RdYlGn")[11:1] #with green to red gradient
+nbbrak<-11 #depend on the coloor chosen
+as.numeric(cut(SurvProp$PMinf,breaks=nbbrak))
+plot(SurvProp[,c(3,2)],xlim=c(0,100),ylim=c(0,100),bty="n",
+     las=1,pch=21,cex=3,col="black",xaxt="n",yaxt="n",
+     bg=coloor[as.numeric(cut(SurvProp$PMinf,breaks=nbbrak))],
+     ylab="Natural treatment survival rate (%)",
+     xlab="Protected treatment survival rate (%)",
+     font.lab=2,cex.lab=1.5)
+text(SurvProp[,c(3,2)],labels=SurvProp$Progeny)
+abline(0,1,lty=5,col=grey(0.6,1),lwd=3)
+legend(8,98,legend=levels(cut(SurvProp$PMinf,breaks=nbbrak)),
+       bty="n",fill=coloor,title="Mean\nPM infection")
+axis(1,lwd=2,cex.axis=1,las=1,font=2)
+axis(2,lwd=2,las=1,font=2)
+box(lwd=2)
+par(op)
+dev.off()
 
 
 ##############################################################################/
