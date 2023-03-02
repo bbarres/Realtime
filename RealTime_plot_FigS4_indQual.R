33a556cb21aedf99af44043e048102ad063af082
bbarres <benoit.barres@gmail.com>
2023-03-03 00:56:03 +0100

    picking colors for S4 figure

--- RealTime_plot_FigS4_indQual.R
+++ RealTime_plot_FigS4_indQual.R
@@ -4,7 +4,7 @@
 ##############################################################################/
 ##############################################################################/
 
-
+library(RColorBrewer)
 #loading and preparing the data set
 genoQual<-read.table("data/dataSup/RT_s_pre_ST.txt",
                   sep="\t",header=T,dec=".")
@@ -12,11 +12,12 @@ genoQual<-read.table("data/dataSup/RT_s_pre_ST.txt",
 summary(genoQual)
 colnames(genoQual)
 
-
+http://127.0.0.1:17593/graphics/plot_zoom_png?width=2498&height=1418
 ##############################################################################/
 #plotting Figure S4####
 ##############################################################################/
 
+#defining the threshold
 seuilp50min<-(mean(genoQual$p50.GC,na.rm=T)-0.01)
 seuilp10min<-(mean(genoQual$p10.GC,na.rm=T)-0.015)
 seuilCRatemin<-(mean(genoQual$Call.Rate,na.rm=T)-0.01)
@@ -26,6 +27,11 @@ xrange<-c(min(genoQual$Call.Rate[genoQual$Call.Rate!=0],
               na.rm=T)+0.01)
 yrange<-c(min(genoQual$p10.GC,na.rm=T)-0.01,
           max(genoQual$p10.GC,na.rm=T)+0.01)
+#defining a color vector
+coVec<-c(rgb(0,0,0,max=255,alpha=50),
+         rgb(228,26,26,max=255,alpha=50),
+         rgb(55,126,184,max=255,alpha=50),
+         rgb(77,175,74,max=255,alpha=50))
 
 
 #plotting Figure S4
@@ -34,16 +40,18 @@ op<-par(mfrow=c(1,2))
 
 plot(p10.GC~Call.Rate,xlim=xrange,ylim=yrange,
      data=genoQual[genoQual$p50.GC>=seuilp50min & 
-                     genoQual$p10.GC>=seuilp10min,])
-abline(h=c(seuilp10min),lty=2,col="green4")
-abline(v=c(seuilCRatemin),lty=2,col="blue")
+                     genoQual$p10.GC>=seuilp10min &
+                     genoQual$Call.Rate>=seuilCRatemin,],
+     col=coVec[1],pch=19,cex=2)
+abline(h=c(seuilp10min),lty=2,col=brewer.pal(9,"Set1")[2],lwd=3)
+abline(v=c(seuilCRatemin),lty=2,col=brewer.pal(9,"Set1")[3],lwd=3)
 points((p10.GC[p50.GC<seuilp50min])~(Call.Rate[p50.GC<seuilp50min]),
-       col="red",pch=1,cex=0.9,data=genoQual)
+       col=coVec[2],pch=19,cex=2,data=genoQual)
 points((p10.GC[p10.GC<seuilp10min])~(Call.Rate[p10.GC<seuilp10min]),
-       col="green4",pch=1,cex=0.7,data=genoQual)
+       col=coVec[3],pch=19,cex=2,data=genoQual)
 points((p10.GC[Call.Rate<seuilCRatemin])~
          (Call.Rate[Call.Rate<seuilCRatemin]),
-       col="blue",pch=1,cex=0.5,data=genoQual)
+       col=coVec[4],pch=19,cex=2,data=genoQual)
 
 plot(p50.GC~Call.Rate,xlim=xrange)
 abline(h=c(seuilp50min),lty=2,col="red")
