86623d2961a34b3ea5bccb7a64e86bebf2298ec3
bbarres <benoit.barres@gmail.com>
2023-03-18 23:27:01 +0100

    cleaning the script of Figure S4

--- RealTime_plot_GoldGate_SNP.R
+++ RealTime_plot_GoldGate_SNP.R
@@ -14,56 +14,55 @@ source("RealTime_load.R")
 
 #Functions to draw an ellipse. The following code has been retrieved from a 
 #function written by Peter D. M. Macdonald of McMaster University
-ellipse<-function (hlaxa = 1, hlaxb = 1, theta = 0, xc = 0, yc = 0, newplot = F,
-npoints = 100, ...)
+ellipse<-function(hlaxa=1,hlaxb=1,theta=0,xc=0,yc=0,newplot=F,npoints=100,...)
 {
-	a <- seq(0, 2 * pi, length = npoints + 1)
-	x <- hlaxa * cos(a)
-	y <- hlaxb * sin(a)
-	alpha <- angle(x, y)
-	rad <- sqrt(x^2 + y^2)
-	xp <- rad * cos(alpha + theta) + xc
-	yp <- rad * sin(alpha + theta) + yc
-	if (newplot)
-		plot(xp, yp, type = "l", ...)
-	else lines(xp, yp, ...)
-	invisible()
+  a<-seq(0,2*pi,length=npoints+1)
+  x<-hlaxa*cos(a)
+  y<-hlaxb*sin(a)
+  alpha<-angle(x,y)
+  rad<-sqrt(x^2+y^2)
+  xp<-rad*cos(alpha+theta)+xc
+  yp<-rad*sin(alpha+theta)+yc
+  if (newplot)
+    plot(xp,yp,type="l",...)
+  elselines(xp,yp,...)
+  invisible()
 }
 
-angle<-function (x, y)
+angle<-function(x,y)
 {
-	angle2 <- function(xy) {
-		x <- xy[1]
-		y <- xy[2]
-		if (x > 0) {
-			atan(y/x)
-		}
-		else {
-			if (x < 0 & y != 0) {
-				atan(y/x) + sign(y) * pi
-			}
-			else {
-				if (x < 0 & y == 0) {
-					pi
-				}
-				else {
-					if (y != 0) {
-						(sign(y) * pi)/2
-					}
-					else {
-						NA
-					}
-				}
-			}
-		}
-	}
-	apply(cbind(x, y), 1, angle2)
-
+  angle2<-function(xy) {
+    x<-xy[1]
+    y<-xy[2]
+    if(x>0) {
+      atan(y/x)
+    }
+    else {
+      if (x<0&y!=0) {
+        atan(y/x)+sign(y)*pi
+      }
+      else {
+        if (x<0&y==0) {
+          pi
+        }
+        else {
+          if(y!=0) {
+            (sign(y)*pi)/2
+          }
+          else {
+            NA
+          }
+        }
+      }
+    }
+  }
+  apply(cbind(x,y),1,angle2)
+  
 }
 
 
 ##############################################################################/
-#Data formating function####
+#Data formatting function####
 ##############################################################################/
 
 
