496ec0564c98d2ba2537adfe2a03d3d7513e3125
bbarres <benoit.barres@gmail.com>
2023-04-28 16:34:53 +0200

    final name of Figure modifications in the script

--- RealTime_plot_SurvShanon.R
+++ RealTime_plot_SurvShanon.R
@@ -8,7 +8,7 @@ source("RealTime_load.R")
 
 
 ##############################################################################/
-#yearly evolution of the shannon diversity index by bloc####
+#Figure 7: yearly evolution of the shannon diversity index by bloc####
 ##############################################################################/
 
 #limiting the data set to useful data
@@ -55,7 +55,7 @@ row.names(Shanplot)<-c("Emerging","2009","2010","2011",
                        "2012","2013","2014","2015","2016",
                        "2017")
 
-pdf(file="output/Figure_ShannonEvol.pdf",width=7,height=6)
+pdf(file="output/Figure_7_ShannonEvol.pdf",width=7,height=6)
 op<-par(mar=c(5.1,4.3,1.1,1.1))
 matplot(Shanplot,type="b",ylim=c(3.25,3.85),las=1,
         col=colovec[c(1,2,1,1,1,2,2,1,1)],lwd=2,
@@ -78,59 +78,6 @@ legend(1,3.35,legend="Protected treatment",
        bty="n",cex=1.3,title.cex=1,y.intersp=1.5,x.intersp=0.5,lwd=2)
 par(op)
 #export to .pdf 7 x 6 inches
-dev.off()
-
-
-##############################################################################/
-#Figure 5: progeny survival by treatment and powdery mildew infection####
-##############################################################################/
-
-#preparing the data set
-progSurv<-dispo[dispo$family_simp!="CC" & dispo$family_simp!="hd" & 
-                  !is.na(dispo$an_mort) & dispo$an_mort!="g" & 
-                  dispo$family_simp!="26",
-                c("Sample_ID","bloc","PU","family_simp",
-                  "treat","an_mort","oid_moy")]
-levels(progSurv$family_simp)[1:2]<-c("01","09")
-progSurv<-drop.levels(progSurv)
-progSurv$DoA<-progSurv$an_mort
-progSurv[progSurv$DoA!="vivant",]$DoA<-"dead"
-progSurv[progSurv$DoA=="vivant",]$DoA<-"alive"
-progSurv$DoA<-as.factor(progSurv$DoA)
-progSurv$treat<-as.factor(progSurv$treat)
-
-#computing the survival rate by family by treatment
-SurvProp<-as.data.frame(prop.table(table(progSurv$treat,
-                                         progSurv$DoA,
-                                         progSurv$family_simp),
-                     margin=c(1,3))*100)
-SurvProp<-SurvProp[SurvProp$Var2=="alive",]
-SurvProp<-pivot_wider(SurvProp[,-c(2)],names_from=Var1,values_from=Freq)
-colnames(SurvProp)<-c("Progeny","Natural","Protected")
-SurvProp$PMinf<-tapply(progSurv$oid_moy,INDEX=progSurv$family_simp,
-                       FUN=mean,na.rm=TRUE)
-
-#ploting the results
-pdf(file="output/Figure5_SurvByProg.pdf",width=7,height=7)
-op<-par(mar=c(5.1,4.3,1,1))
-coloor<-brewer.pal(11,"RdYlGn")[c(11:9,7,5,3:1)] #with green to red gradient
-#another color gradient brewer.pal(9,"YlOrRd") #with yellow to red gradient
-nbbrak<-c(20,21,22,23,24,25,26,27,28)
-plot(SurvProp[,c(3,2)],xlim=c(0,100),ylim=c(0,100),bty="n",
-     las=1,pch=21,cex=3,col="black",xaxt="n",yaxt="n",
-     bg=coloor[as.numeric(cut(SurvProp$PMinf,breaks=nbbrak))],
-     ylab="Natural treatment survival rate (%)",
-     xlab="Protected treatment survival rate (%)",
-     font.lab=2,cex.lab=1.5)
-text(SurvProp[,c(3,2)],labels=SurvProp$Progeny)
-abline(0,1,lty=5,col=grey(0.6,1),lwd=3)
-legend(8,98,legend=c("]20-21]","]21-22]","]22-23]","]23-24]",
-                    "]24-25]","]25-26]","]26-27]","]27-28]"),
-       bty="n",fill=coloor,title="Mean\nPM infection")
-axis(1,lwd=2,cex.axis=1,las=1,font=2)
-axis(2,lwd=2,las=1,font=2)
-box(lwd=2)
-par(op)
 dev.off()
 
 
