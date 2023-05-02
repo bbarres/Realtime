6329a0d82c7b71c558970047f98cbc03472a0225
bbarres <benoit.barres@gmail.com>
2023-05-02 13:08:47 +0200

    improving the glm results figures

--- RealTime_plot_survMildew.R
+++ RealTime_plot_survMildew.R
@@ -7,11 +7,7 @@
 #loading the necessary packages and data sets
 source("RealTime_load.R")
 
-
-##############################################################################/
-#Model 1: effect of acorn weight and treatment####
-##############################################################################/
-
+#preparing the dataset
 temp<-RTdata[RTdata$family_simp!="PAR" & RTdata$family_simp!="CC" &
                RTdata$family_simp!="hd" & !is.na(RTdata$statut10),]
 temp<-temp[,c("treat","Sample_ID","family_simp","oid4_09","oid5_10",
@@ -24,6 +20,11 @@ colnames(temp)<-c("Treatment","Taxa","family_simp","oid4_09","oid5_10",
 temp$Treatment<-as.factor(temp$Treatment)
 levels(temp$Treatment)<-c("Natural","Protected")
 
+
+##############################################################################/
+#Model 1: effect of acorn weight and treatment####
+##############################################################################/
+
 survTreatAndAcorn<-glm(Survival~Treatment+Acorn_weight,
                        data=temp,family=binomial)
 
@@ -32,21 +33,23 @@ survTreatAndAcorn<-glm(Survival~Treatment+Acorn_weight,
 #Figure SXX: plot using visreg package####
 ##############################################################################/
 
+pdf(file="output/Figure_SXX_GLMacorn.pdf",width=7,height=5)
+op<-par(mar=c(5.1,4.1,1.1,1.1))
 #defining a color vector
 colovec<-c(brewer.pal(12,"Paired")[1:2],brewer.pal(12,"Paired")[3:4],
            brewer.pal(11,"RdYlGn")[1])
-pdf(file="output/Figure_SXX_GLMacorn.pdf",width=7,height=5)
-op<-par(mar=c(5.1,4.1,1.1,1.1))
 visreg(survTreatAndAcorn,"Acorn_weight",scale="response",
-       rug=2,ylim=c(0,1),by="Treatment",overlay=TRUE,
-       xlab="Acorn weight",
+       rug=FALSE,ylim=c(0,1),
+       by="Treatment",overlay=TRUE,
+       xlab="Acorn weight (g)",
        ylab="Survival (2017)",
        line=list(col=colovec[c(4,2)]),
        fill=list(col=colovec[c(3,1)]))
+#defining a color vector
 colovec<-c(brewer.pal(12,"Set3")[6:7],
            brewer.pal(9,"Set1")[1:2])
-rug(temp[temp$Survival==0,]$Acorn_weight,side=1,col=colovec[1])
-rug(temp[temp$Survival==1,]$Acorn_weight,side=3,col=colovec[2])
+rug(jitter(temp[temp$Survival==0,]$Acorn_weight),side=1,col=colovec[1])
+rug(jitter(temp[temp$Survival==1,]$Acorn_weight),side=3,col=colovec[2])
 box()
 par(op)
 #export to pdf 7 x 5 inches
@@ -65,10 +68,11 @@ survOid<-glm(Survival~Acorn_weight+Powdery_mildew,
 #Figure 3: plot using visreg package####
 ##############################################################################/
 
-colovec<-c(brewer.pal(12,"Set3")[6:7],
-           brewer.pal(9,"Set1")[1:2])
 pdf(file="output/Figure_3_GLMmildew.pdf",width=7,height=5)
 op<-par(mar=c(5.1,4.1,1.1,1.1))
+#defining a color vector
+colovec<-c(brewer.pal(12,"Set3")[6:7],
+           brewer.pal(9,"Set1")[1:2])
 visreg(survOid,"Powdery_mildew",scale="response",rug=2,ylim=c(0,1),
        xlab="Mean infection (2009-2013) (%)",
        ylab="Survival (2017)")
@@ -85,18 +89,18 @@ dev.off()
 ##############################################################################/
 
 survH09<-glm(Survival~H09v*Treatment,
-                       data=temp,family=binomial)
+             data=temp,family=binomial)
 
 
 ##############################################################################/
-#Figure SXX: plot using visreg package####
+#Figure SXX: plot using visreg Height in 2009 effect visualisation ####
 ##############################################################################/
 
+pdf(file="output/Figure_SXX_GLMH09.pdf",width=7,height=5)
+op<-par(mar=c(5.1,4.1,1.1,1.1))
 #defining a color vector
 colovec<-c(brewer.pal(12,"Paired")[1:2],brewer.pal(12,"Paired")[3:4],
            brewer.pal(11,"RdYlGn")[1])
-pdf(file="output/Figure_SXX_GLMH09.pdf",width=7,height=5)
-op<-par(mar=c(5.1,4.1,1.1,1.1))
 visreg(survH09,"H09v",scale="response",xlim=c(0,110),
        rug=2,ylim=c(0,1),by="Treatment",overlay=TRUE,
        xlab="Height in 2009",
@@ -118,18 +122,18 @@ dev.off()
 ##############################################################################/
 
 survFrost<-glm(Survival~Acorn_weight+Powdery_mildew+Frost_damage,
-             data=temp,family=binomial)
+               data=temp,family=binomial)
 
 
 ##############################################################################/
-#Figure SXX: plot using visreg package####
+#Figure SXX: plot using visreg package for Frost damage visualisation####
 ##############################################################################/
 
+pdf(file="output/Figure_SXX_GLMFrost.pdf",width=7,height=5)
+op<-par(mar=c(5.1,4.1,1.1,1.1))
 #defining a color vector
 colovec<-c(brewer.pal(12,"Paired")[1:2],brewer.pal(12,"Paired")[3:4],
            brewer.pal(11,"RdYlGn")[1])
-pdf(file="output/Figure_SXX_GLMFrost.pdf",width=7,height=5)
-op<-par(mar=c(5.1,4.1,1.1,1.1))
 visreg(survFrost,"Powdery_mildew",scale="response",
        rug=2,ylim=c(0,1),by="Frost_damage",overlay=TRUE,
        xlab="Mean infection (2009-2013) (%)",
@@ -149,19 +153,19 @@ dev.off()
 ##############################################################################/
 
 survFull<-glm(Survival~Acorn_weight+Powdery_mildew*family_simp+Frost_damage,
-               data=temp,family=binomial)
+              data=temp,family=binomial)
 
 
 ##############################################################################/
 #Figure SXX: plot using visreg package####
 ##############################################################################/
 
+pdf(file="output/Figure_SXX_GLMFrost.pdf",width=7,height=5)
+op<-par(mar=c(5.1,4.1,1.1,1.1))
 #defining a color vector
 colovec<-c(brewer.pal(12,"Paired")[1:2],brewer.pal(12,"Paired")[3:4],
            brewer.pal(11,"RdYlGn")[1])
-pdf(file="output/Figure_SXX_GLMFrost.pdf",width=7,height=5)
-op<-par(mar=c(5.1,4.1,1.1,1.1))
-visreg(survFrost,"Powdery_mildew",scale="response",
+visreg(survFull,"Powdery_mildew",scale="response",
        rug=2,ylim=c(0,1),by="Frost_damage",overlay=TRUE,
        xlab="Mean infection (2009-2013) (%)",
        ylab="Survival (2017)")
